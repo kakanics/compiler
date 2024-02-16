1c53082eae3f7f024ef8482b1a91f9be58283bb3
kakanics <affan.bb7@gmail.com>
2024-02-16 14:43:25 +0500

    categorization function

--- lexicalAnalyser.py
+++ lexicalAnalyser.py
@@ -1,4 +1,8 @@
+import re
+
 # This function breaks the input into tokens
+
+
 def tokenize(inp):
     sol = []
     token = ""
@@ -32,4 +36,23 @@ def tokenize(inp):
 
 # This functions categorizes the tokens into keywords, special symbols, identifiers etc
 # uses regex
+
+
 def categorize(tokens):
+    sol = []
+    for i in tokens:
+        if re.match(r';|\(|\)|\{|\}|\=', i):
+            sol.append(("Special Symbol"))
+        elif re.match(r"int|float|char|void|main|if|else|while|return|for|while", i):
+            sol.append("Keyword")
+        elif re.match(r"[a-zA-Z_][a-zA-Z0-9_]*", i):
+            sol.append("Identifier")
+        elif re.match(r"[0-9]+", i):
+            sol.append("Number")
+        elif re.match(r"==|!=|>|<|>=|<=|&&|\|\||\+|\-|\*|%", i):
+            sol.append("Operator")
+        elif re.match(r"pi,g", i):
+            sol.append("Constant")
+        else:
+            return Exception("Invalid token: " + i)
+    return sol
